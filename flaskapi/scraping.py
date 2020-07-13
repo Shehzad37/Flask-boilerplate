daff5d88fbb3ab81bba0c5f16a82563de8c60674
Shehzad Abbas <Shehzad37@gmail.com>
2020-07-13 19:15:29 +0200

    Update scraping.py

--- flaskapi/scraping.py
+++ flaskapi/scraping.py
@@ -1,6 +1,6 @@
 import requests
 
-resp =  requests.get('http://api.openweathermap.org/data/2.5/weather?zip=24106,de&appid=58c4e41bd6b01af1061714aad969ee0d')
+resp =  requests.get('http://api.openweathermap.org/data/2.5/weather?zip=24106,de&appid=APIKEY')
 # print(dir(resp))
 #json = resp.json()
 # print(json)
